8318d122a8585b4869dc193b1521586802e67b20
IvnoGood <afonsovic2009@gmail.com>
2025-07-24 23:44:50 +0100

    added margin to fit the gap used in alarm cards

--- components/ui/EmptyFetch.jsx
+++ components/ui/EmptyFetch.jsx
@@ -4,7 +4,7 @@ const EmptyFetch = ({ title, color }) => {
     const theme = useTheme()
     const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
     return (
-        <Card style={[{ flex: 1, minHeight: 100, alignContent: 'center', paddingVertical: 30, backgroundColor: theme.colors.surfaceVariant }]}>
+        <Card style={[{ flex: 1, marginTop: 15, minHeight: 100, alignContent: 'center', paddingVertical: 30, backgroundColor: theme.colors.surfaceVariant }]}>
             <Card.Title title={title} titleVariant='titleLarge' titleStyle={{ textAlign: 'center' }} />
         </Card>
     )
