432ecbc0b81e7ac09ff60ee606501a91511dff20
IvnoGood <afonsovic2009@gmail.com>
2025-07-29 17:17:45 +0100

    removed card on empty favs
    removed empty card that loaded for nothing

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -120,7 +120,7 @@ export default function HomeScreen() {
                                 />
                             </View>
                         </View>
-                    )) : <EmptyFetch title={'No favorite alarm'} />}
+                    )) : <></>}
                 </ScrollView>
             </SafeAreaView>
             {showNewDevice ? (<></>) : (
